646bb4456ac61e4117aac848690265e88eed8ca4
Bigboss <silvekerhernandez@proton.me>
2024-11-27 19:56:07 -0500

    infoBooks update repository FD

---
+++ src/main/java/com/aluracursos/infoBooks/InfoBooksApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.infoBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/infoBooks/InfoBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.infoBooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
